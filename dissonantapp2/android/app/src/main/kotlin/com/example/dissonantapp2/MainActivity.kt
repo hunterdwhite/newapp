c15b8d1d3b7473f8da7749359a886ec92f8bca4b
BenLewis42 <60411698+hunterdwhite@users.noreply.github.com>
2025-07-22 23:32:42 -0400

    merge

--- dissonantapp2/android/app/src/main/kotlin/com/example/dissonantapp2/MainActivity.kt
+++
@@ -1,8 +0,0 @@
-package com.dissonant.dissonantapp2
-
-import io.flutter.embedding.android.FlutterFragmentActivity
-
-class MainActivity : FlutterFragmentActivity() {
-    // No manual plugin registration calls, 
-    // letting the new embedding automatically register everything
-}
